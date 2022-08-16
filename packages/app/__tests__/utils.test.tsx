d23650d86478869d686f97bd1b72bb59fe23e87c
Arda Satata <ardasatata@gmail.com>
2022-08-16 23:34:27 +0700

    Changes to be committed: 	modified:   .gitignore 	new file:   .yarn/install-state.gz 	new file:   .yarn/releases/yarn-berry.cjs 	modified:   packages/app/__tests__/utils.test.tsx

--- packages/app/__tests__/utils.test.tsx
+++ packages/app/__tests__/utils.test.tsx
@@ -1,11 +1,9 @@
-import {shuffle} from "../src/utils";
+import {randomizedCards, shuffle} from "../src/utils";
 import {CARD_PAIRS_VALUE} from "../src/screens/game/consts";
 
 test('shuffle function return the same length', async () => {
 
-  const cards = shuffle(CARD_PAIRS_VALUE)
-  const duplicated_cards = [...cards, ...cards].map((card, i) => ({ ...card, key: `${card.key}-${i}` }))
-  const shuffled =  shuffle(duplicated_cards)
+  const cards = randomizedCards()
 
-  expect(shuffled.length).toBe(duplicated_cards.length)
+  expect(cards.length).toBe(CARD_PAIRS_VALUE.length * 2)
 })
